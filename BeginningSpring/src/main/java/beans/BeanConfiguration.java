dc21ff1c8cde2962faa203f2e958c516233fc73b
Charwillbro <35745538+Charwillbro@users.noreply.github.com>
2018-02-19 16:29:25 -0600

    -Installed JDK and made it the default jre in window->preferences->java->jre
    cleaned the project
    ->project->Clean...
    
    and updated maven. no beans

--- BeginningSpring/src/main/java/beans/BeanConfiguration.java
+++ BeginningSpring/src/main/java/beans/BeanConfiguration.java
@@ -1,7 +1,8 @@
 package beans;
 
 import org.springframework.context.annotation.Bean;
-import	org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfiguration {
